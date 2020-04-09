f9dfaba0da4f25cad002e07042b76a3a0ebbb427
Indika Piyasena <indika.piyasena@gmail.com>
2020-04-09 21:02:09 +1000

    add more machines

--- src/mainchain/const.py
+++ src/mainchain/const.py
@@ -12,5 +12,17 @@ MACHINES = {
     'shark': {
         'service': 'und',
         'home': Path('/home/deploy/.und_mainchain')
+    },
+    'node1': {
+        'service': 'node1',
+        'home': Path('/home/deploy/node1/.und_mainchain')
+    },
+    'node2': {
+        'service': 'node2',
+        'home': Path('/home/deploy/node2/.und_mainchain')
+    },
+    'node3': {
+        'service': 'node3',
+        'home': Path('/home/deploy/node3/.und_mainchain')
     }
 }
