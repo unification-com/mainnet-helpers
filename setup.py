ee7f8258e7e0ddef1979570fc8c7b04b077562c7
Indika Piyasena <indika.piyasena@gmail.com>
2020-04-10 19:58:23 +1000

    just tweaking

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = [
 setuptools.setup(
     name="undmainchain",
     packages=['undmainchain'],
-    version="0.0.6",
+    version="0.0.7",
     author="Unification Foundation",
     author_email="indika@unification.com",
     description="Helper tools for administering the Unification Mainchain",
