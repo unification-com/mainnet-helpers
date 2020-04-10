babee141ef2997ca2123a1f1ae8a35bd12ba88b1
Indika Piyasena <indika.piyasena@gmail.com>
2020-04-10 19:33:24 +1000

    remove empty file

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = [
 setuptools.setup(
     name="undmainchain",
     packages=['undmainchain'],
-    version="0.0.5",
+    version="0.0.6",
     author="Unification Foundation",
     author_email="indika@unification.com",
     description="Helper tools for administering the Unification Mainchain",
